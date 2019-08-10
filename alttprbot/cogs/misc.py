6660de17c4b1b3daf2486a624fd8e6fe2adcbf7d
Thomas Prescott <tcprescott@gmail.com>
2019-08-09 22:13:41 -0500

    pedestal goal lol

--- alttprbot/cogs/misc.py
+++ alttprbot/cogs/misc.py
@@ -12,6 +12,10 @@ class Misc(commands.Cog):
     async def group_list(self, ctx, member: discord.Member):
         await ctx.send(member.joined_at)
 
+    @commands.command(hidden=True)
+    async def pedestalgoal(self, ctx):
+        await ctx.send("> If it takes 2 hours its because GT is required, which really isn't a thing anymore in pedestal goal games\n-Synack")
+    
     @commands.command()
     async def crc32(self, ctx):
         await ctx.send("If you need help verifying your ROM file needed for ALTTPR, check this out: http://alttp.mymm1.com/game/checkcrc/\nIt can also tell you the permalink of an already randomized game!")
