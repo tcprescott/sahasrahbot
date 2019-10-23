64f01cd9774a4313429691542263ddbff0b70fb7
Thomas Prescott <tcprescott@gmail.com>
2019-10-22 21:47:06 -0500

    better error logging, send me a DM on unhandled errors

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import asyncio
 import discord
 from discord.ext import commands
 import importlib
+import traceback
 
 # from alttprbot import reactionrole
 from alttprbot.util import orm
@@ -26,22 +27,30 @@ discordbot.load_extension("alttprbot.cogs.tournament")
 if importlib.util.find_spec('jishaku'):
     discordbot.load_extension('jishaku')
 
-if not c.DEBUG:
-    @discordbot.event
-    async def on_command_error(ctx, error):
-        if isinstance(error, commands.CheckFailure):
-            await ctx.message.add_reaction('🚫')
-        if isinstance(error, commands.errors.MissingPermissions):
-            await ctx.message.add_reaction('🚫')
-        elif isinstance(error, commands.CommandNotFound):
-            pass
-        elif isinstance(error, commands.errors.MissingRequiredArgument):
-            await ctx.send(error)
-            await ctx.message.add_reaction('👎')
+
+@discordbot.event
+async def on_command_error(ctx, error):
+    await ctx.message.remove_reaction('⌚', ctx.bot.user)
+    if isinstance(error, commands.CheckFailure):
+        await ctx.message.add_reaction('🚫')
+    if isinstance(error, commands.errors.MissingPermissions):
+        await ctx.message.add_reaction('🚫')
+    elif isinstance(error, commands.CommandNotFound):
+        pass
+    elif isinstance(error, commands.errors.MissingRequiredArgument):
+        await ctx.send(error)
+        await ctx.message.add_reaction('👎')
+    else:
+        traceback_data = ''.join(traceback.format_exception(type(error), error, error.__traceback__, 1))
+        formatted_traceback = f'User: {ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})\nGuild: {ctx.guild.name} ({ctx.guild.id})\nChannel: {ctx.channel.name} ({ctx.channel.id})\nMessage: {ctx.message.clean_content}\n```python\n{traceback_data}```'
+        if c.DEBUG:
+            await ctx.send(formatted_traceback)
         else:
+            app = await ctx.bot.application_info()
+            await app.owner.send(formatted_traceback)
             await ctx.send(error)
-            await ctx.message.add_reaction('👎')
-        await ctx.message.remove_reaction('⌚', ctx.bot.user)
+        
+        await ctx.message.add_reaction('👎')
 
 
 @discordbot.event
