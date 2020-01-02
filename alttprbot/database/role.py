ca41b73fa4a92b9f84bda0627a195d142ee5b987
Thomas Prescott <tcprescott@gmail.com>
2020-01-01 18:11:06 -0600

    cache database queries to role tables

--- alttprbot/database/role.py
+++ alttprbot/database/role.py
@@ -1,6 +1,9 @@
+import aiocache
+
 from ..util import orm
 
 
+@aiocache.cached(ttl=300, cache=aiocache.SimpleMemoryCache, namespace="role")
 async def get_role_by_group_emoji(channel_id, message_id, emoji, guild_id):
     roles = await orm.select(
         'SELECT rr.role_id from reaction_group rg LEFT JOIN reaction_role rr ON rr.reaction_group_id = rg.id WHERE rg.channel_id=%s AND rg.message_id=%s AND rg.guild_id=%s and rr.emoji=%s;',
@@ -9,6 +12,7 @@ async def get_role_by_group_emoji(channel_id, message_id, emoji, guild_id):
     return roles
 
 
+@aiocache.cached(ttl=300, cache=aiocache.SimpleMemoryCache, namespace="role")
 async def get_guild_groups(guild_id):
     groups = await orm.select(
         'SELECT * from reaction_group WHERE guild_id=%s;',
@@ -17,6 +21,7 @@ async def get_guild_groups(guild_id):
     return groups
 
 
+@aiocache.cached(ttl=300, cache=aiocache.SimpleMemoryCache, namespace="role")
 async def get_guild_group_by_id(reaction_group_id, guild_id):
     groups = await orm.select(
         'SELECT * from reaction_group WHERE id=%s AND guild_id=%s;',
@@ -25,6 +30,7 @@ async def get_guild_group_by_id(reaction_group_id, guild_id):
     return groups
 
 
+@aiocache.cached(ttl=300, cache=aiocache.SimpleMemoryCache, namespace="role")
 async def get_group_roles(reaction_group_id, guild_id):
     roles = await orm.select(
         'SELECT * from reaction_role where reaction_group_id=%s AND guild_id=%s;',
@@ -33,6 +39,7 @@ async def get_group_roles(reaction_group_id, guild_id):
     return roles
 
 
+@aiocache.cached(ttl=300, cache=aiocache.SimpleMemoryCache, namespace="role")
 async def get_role(reaction_role_id, guild_id):
     role = await orm.select(
         'SELECT * from reaction_role where id = %s and guild_id = %s;',
@@ -41,6 +48,7 @@ async def get_role(reaction_role_id, guild_id):
     return role[0]
 
 
+@aiocache.cached(ttl=300, cache=aiocache.SimpleMemoryCache, namespace="role")
 async def get_role_group(reaction_role_id, guild_id):
     role = await orm.select(
         'SELECT rg.id, rg.guild_id, rg.channel_id, rg.message_id, rr.emoji '
@@ -66,6 +74,7 @@ async def create_group(guild_id, channel_id, message_id, name, description, bot_
         'INSERT into reaction_group (`guild_id`,`channel_id`,`message_id`,`name`,`description`,`bot_managed`) values (%s, %s, %s, %s, %s, %s)',
         [guild_id, channel_id, message_id, name, description, bot_managed]
     )
+    await aiocache.SimpleMemoryCache().clear(namespace="role")
 
 
 async def delete_group(guild_id, group_id):
@@ -73,6 +82,7 @@ async def delete_group(guild_id, group_id):
         'DELETE FROM reaction_group WHERE guild_id=%s AND id=%s',
         [guild_id, group_id]
     )
+    await aiocache.SimpleMemoryCache().clear(namespace="role")
 
 
 async def update_group(guild_id, group_id, name, description):
@@ -80,6 +90,7 @@ async def update_group(guild_id, group_id, name, description):
         'UPDATE reaction_group SET name=%s, description=%s WHERE guild_id=%s AND id=%s',
         [name, description, guild_id, group_id]
     )
+    await aiocache.SimpleMemoryCache().clear(namespace="role")
 
 
 async def create_role(guild_id, reaction_group_id, role_id, name, emoji, description, protect_mentions: int):
@@ -87,6 +98,7 @@ async def create_role(guild_id, reaction_group_id, role_id, name, emoji, descrip
         'SELECT id from reaction_group WHERE id = %s and guild_id = %s;',
         [reaction_group_id, guild_id]
     )
+    await aiocache.SimpleMemoryCache().clear(namespace="role")
 
     existing_roles = await orm.select(
         'SELECT id from reaction_role WHERE emoji = %s and reaction_group_id = %s',
@@ -100,6 +112,7 @@ async def create_role(guild_id, reaction_group_id, role_id, name, emoji, descrip
         'INSERT into reaction_role (`guild_id`, `reaction_group_id`, `role_id`, `name`, `emoji`, `description`, `protect_mentions`) values (%s, %s, %s, %s, %s, %s, %s)',
         [guild_id, reaction_group_id, role_id, name, emoji, description, protect_mentions]
     )
+    await aiocache.SimpleMemoryCache().clear(namespace="role")
 
 
 async def delete_role(guild_id, role_id):
@@ -107,6 +120,7 @@ async def delete_role(guild_id, role_id):
         'DELETE FROM reaction_role WHERE guild_id=%s AND id=%s',
         [guild_id, role_id]
     )
+    await aiocache.SimpleMemoryCache().clear(namespace="role")
 
 
 async def update_role(guild_id, role_id, name, description, protect_mentions: int):
@@ -114,6 +128,7 @@ async def update_role(guild_id, role_id, name, description, protect_mentions: in
         'UPDATE reaction_role SET name=%s, description=%s, protect_mentions=%s WHERE guild_id=%s AND id=%s',
         [name, description, protect_mentions, guild_id, role_id]
     )
+    await aiocache.SimpleMemoryCache().clear(namespace="role")
 
 
 async def increment_mention_count(guild_id, role_id):
@@ -121,3 +136,4 @@ async def increment_mention_count(guild_id, role_id):
         'INSERT INTO mention_counters(guild_id,role_id) VALUES (%s,%s) ON DUPLICATE KEY UPDATE counter = counter+1;',
         [guild_id, role_id]
     )
+    await aiocache.SimpleMemoryCache().clear(namespace="role")
