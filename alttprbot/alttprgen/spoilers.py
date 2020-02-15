1ae486a0eaebc20cca40bb4558175a645237b0df
Thomas Prescott <tcprescott@gmail.com>
2020-02-15 17:27:14 -0600

    translate dungeon items in spoiler log

--- alttprbot/alttprgen/spoilers.py
+++ alttprbot/alttprgen/spoilers.py
@@ -20,7 +20,7 @@ async def write_json_to_disk(seed):
     code = await seed.code()
     filename = f"spoiler__{seed.hash}__{'-'.join(code).replace(' ', '')}__{''.join(random.choices(string.ascii_letters + string.digits, k=4))}.txt"
 
-    sorteddict = seed.get_formatted_spoiler()
+    sorteddict = seed.get_formatted_spoiler(translate_dungeon_items=True)
 
     async with aiofiles.open(f"{c.SpoilerLogLocal}/{filename}", "w+", newline='\r\n') as out:
         await out.write(json.dumps(sorteddict, indent=4))
