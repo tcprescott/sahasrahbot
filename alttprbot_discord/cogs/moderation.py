daceb9dc23cd390a5baf2bc14ae49f670bbc7252
Thomas Prescott <tcprescott@gmail.com>
2020-02-20 22:32:04 -0600

    only delete scripts from members younger than 24 hours delete discord invite links from members younger than 24 hours

--- alttprbot_discord/cogs/moderation.py
+++ alttprbot_discord/cogs/moderation.py
@@ -1,11 +1,17 @@
 import io
 import zipfile
+import datetime
 
+from urllib.parse import urlparse
+from urlextract import URLExtract
+
+import discord
 from discord.ext import commands
 
 from alttprbot.database import config
 from alttprbot.util import http
 
+urlextractor = URLExtract()
 
 class Moderation(commands.Cog):
     def __init__(self, bot):
@@ -13,6 +19,19 @@ class Moderation(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        if isinstance(message.channel, discord.DMChannel):
+            return
+
+        if message.author.joined_at > datetime.datetime.now()-datetime.timedelta(days=1) and await config.get(message.guild.id, 'ModerateNewMemberContent') == "true":
+            for url in urlextractor.gen_urls(message.content):
+                if urlparse(url).netloc in ['discord.gg']:
+                    await message.delete()
+                    await message.channel.send(f'{message.author.mention}, you must be on this server for longer than 24 hours before posting discord invite links.  Please contact a moderator if you want to post an invite link.')
+            for attachment in message.attachments:
+                if attachment.filename.endswith(('.bat', '.exe', '.sh', '.py')):
+                    await message.delete()
+                    await message.channel.send(f'{message.author.mention}, please do not upload executable files.  If your message was deleted in error, please contact a moderator.')
+
         # delete roms if server is configured to do so
         if len(message.attachments) > 0:
             for attachment in message.attachments:
@@ -26,10 +45,7 @@ class Moderation(commands.Cog):
                 elif attachment.filename.endswith(('.sfc', '.smc')) and await config.get(message.guild.id, 'DeleteRoms') == "true":
                     await message.delete()
                     await message.channel.send(f'{message.author.mention}, please do not post ROMs.  If your message was deleted in error, please contact a moderator.')
-
-                elif attachment.filename.endswith(('.bat', '.exe', '.sh', '.py')) and await config.get(message.guild.id, 'DeleteExecutables') == "true":
-                    await message.delete()
-                    await message.channel.send(f'{message.author.mention}, please do not upload executable files.  If your message was deleted in error, please contact a moderator.')
+    
 
 
 async def inspect_zip(url):
