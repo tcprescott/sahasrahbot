c828dc51b3c0bfb24a2b27ab23bf99b88ca0e865
Thomas Prescott <tcprescott@gmail.com>
2020-03-24 18:54:19 -0500

    add support for rolling mystery settings via API

--- alttprbot_api/api.py
+++ alttprbot_api/api.py
@@ -1,6 +1,10 @@
 from quart import Quart, abort, jsonify, request
-from alttprbot_srl import nick_verifier
+
+from pyz3r.mystery import generate_random_settings
+from alttprbot.alttprgen.mystery import get_weights
+from alttprbot.util import http
 from alttprbot_discord.bot import discordbot
+from alttprbot_srl import nick_verifier
 
 sahasrahbotapi = Quart(__name__)
 
@@ -14,14 +18,34 @@ async def verify_srl_user(nick, key):
     else:
         return "Unable to verify your SRL nick.  Please request another key and try again.  Contact Synack if this persists."
 
+@sahasrahbotapi.route('/api/settingsgen/mystery', methods=['POST'])
+async def mysterygen():
+    weights = await request.get_json()
+    settings, customizer = generate_random_settings(weights=weights, spoilers="mystery")
+    return jsonify(
+        settings=settings,
+        customizer=customizer,
+        endpoint='/api/customizer' if customizer else '/api/randomizer'
+    )
+
+@sahasrahbotapi.route('/api/settingsgen/mystery/<string:weightset>', methods=['GET'])
+async def mysterygenwithweights(weightset):
+    weights = await get_weights(weightset)
+    settings, customizer = generate_random_settings(weights=weights, spoilers="mystery")
+    return jsonify(
+        settings=settings,
+        customizer=customizer,
+        endpoint='/api/customizer' if customizer else '/api/randomizer'
+    )
+
 @sahasrahbotapi.errorhandler(400)
 def bad_request(e):
-    return jsonify(success=False, name=e.name, description=e.description, status_code=e.status_code)
+    return jsonify(success=False, error=repr(e))
 
 @sahasrahbotapi.errorhandler(404)
 def not_found(e):
-    return jsonify(success=False, name=e.name, description=e.description, status_code=e.status_code)
+    return jsonify(success=False, error=repr(e))
 
 @sahasrahbotapi.errorhandler(500)
 def something_bad_happened(e):
-    return jsonify(success=False, name=e.name, description=e.description, status_code=e.status_code)
+    return jsonify(success=False, error=repr(e))
