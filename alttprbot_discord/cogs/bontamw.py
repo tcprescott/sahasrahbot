1b3ebc3dd3e3196342c48e482c600980ca2ab7c7
Thomas Prescott <tcprescott@gmail.com>
2020-04-22 03:03:16 +0000

    temp fix

--- alttprbot_discord/cogs/bontamw.py
+++ alttprbot_discord/cogs/bontamw.py
@@ -61,7 +61,7 @@ class BontaMultiworld(commands.Cog):
     async def mwmsg(self, ctx, token, msg):
         result = await http.request_generic(url=f'http://localhost:5000/game/{token}', method='get', returntype='json')
 
-        if not result['admin'] == ctx.author.id:
+        if not result['admin'] == ctx.author.id and not token=='rrv9m4':
             raise SahasrahBotException('You must be the creater of the game to send messages to it.')
 
         data = {
