b160cda4acf3c5e8fd001864a70402f82a1ff825
Thomas Prescott <tcprescott@gmail.com>
2020-07-06 20:40:31 -0500

    add smz3 support

--- alttprbot_racetime/bot.py
+++ alttprbot_racetime/bot.py
@@ -1,13 +1,14 @@
 import logging
-import websockets
-import sys
 import ssl
+import sys
 
-from racetime_bot import Bot
+import websockets
 
 from config import Config as c
+from racetime_bot import Bot
 
 from .handler import AlttprHandler
+from .handler_smz3 import Smz3Handler
 
 logger = logging.getLogger()
 handler = logging.StreamHandler(sys.stdout)
@@ -37,9 +38,36 @@ class AlttprBot(Bot):
         self.loop.create_task(self.reauthorize())
         self.loop.create_task(self.refresh_races())
 
+class Smz3Bot(Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ssl_context = ssl.SSLContext()
+
+    def get_handler_class(self):
+        return Smz3Handler
+
+    def get_handler_kwargs(self, ws_conn, state):
+        return {
+            'conn': ws_conn,
+            'logger': self.logger,
+            'state': state,
+            'command_prefix': c.RACETIME_COMMAND_PREFIX,
+        }
+
+    async def start(self):
+        self.loop.create_task(self.reauthorize())
+        self.loop.create_task(self.refresh_races())
+
 racetime_alttpr = AlttprBot(
     category_slug='ALttPR',
     client_id=c.RACETIME_CLIENT_ID,
     client_secret=c.RACETIME_CLIENT_SECRET,
     logger=logger,
+)
+
+racetime_smz3 = Smz3Bot(
+    category_slug='smz3',
+    client_id=c.RACETIME_CLIENT_ID_SMZ3,
+    client_secret=c.RACETIME_CLIENT_SECRET_SMZ3,
+    logger=logger
 )
---
+++ alttprbot_racetime/handler_smz3.py
@@ -0,0 +1,68 @@
+import asyncio
+import math
+
+from alttprbot.alttprgen import mystery, preset, spoilers
+from alttprbot.database import spoiler_races
+from config import Config as c
+from racetime_bot import RaceHandler
+
+
+class Smz3Handler(RaceHandler):
+    """
+    SahasrahBot race handler. Generates seeds, presets, and frustration.
+    """
+    stop_at = ['cancelled', 'finished']
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.seed_rolled = False
+
+    async def race_data(self, data):
+        self.data = data.get('race')
+
+    async def begin(self):
+        """
+        Send introduction messages.
+        """
+        if not self.state.get('intro_sent') and not c.DEBUG:
+            await self.send_message(
+                f"Hi!  I'm SahasrahBot, your friendly robotic elder and ALTTPR/SMZ3 seed roller! Use {self.command_prefix}help to see what I can do!"
+            )
+            self.state['intro_sent'] = True
+
+    async def ex_preset(self, args, message):
+        await self.roll_game(args, message)
+
+    async def ex_race(self, args, message):
+        await self.roll_game(args, message)
+
+    async def ex_help(self, args, message):
+        await self.send_message("Available commands:\n\"!preset <preset>\" to generate a race preset.")
+
+    async def roll_game(self, args, message):
+        if self.seed_rolled:
+            await self.send_message(
+                'I already rolled a seed!'
+            )
+            return
+        
+        try:
+            preset_name = args[0]
+        except IndexError:
+            await self.send_message(
+                'You must specify a preset!'
+            )
+            return
+
+        await self.send_message("Generating game, please wait.  If nothing happens after a minute, contact Synack.")
+        try:
+            seed, preset_dict = await preset.get_preset(preset_name, randomizer='smz3', spoilers="off")
+        except preset.PresetNotFoundException as e:
+            await self.send_message(str(e))
+            return
+        
+        race_info = f"{preset_name} - {seed.url} - ({seed.code})"
+        await self.set_raceinfo(race_info)
+        await self.send_message(seed.url)
+        await self.send_message("Seed rolling complete.  See race info for details.")
+        self.seed_rolled = True
