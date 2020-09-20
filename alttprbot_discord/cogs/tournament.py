044a5bbb44e58082ed5d212cc511c50fbf6865f7
Thomas Prescott <tcprescott@gmail.com>
2020-09-20 12:17:29 -0500

    dirty fix for DM handling

--- alttprbot_discord/cogs/tournament.py
+++ alttprbot_discord/cogs/tournament.py
@@ -106,7 +106,7 @@ class Tournament(commands.Cog):
                 else:
                     member = await commands.MemberConverter().convert(ctx, player['discordTag'])
                 await member.send(embed=embed)
-            except discord.HTTPException:
+            except Exception:
                 if audit_channel:
                     await audit_channel.send(f"@here Unable to send DM to {player['displayName']}")
                 await ctx.send(f"Unable to send DM to {player['displayName']}")
