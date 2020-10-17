cb0ebb544d8ae10aa35d8597004f814be5d633d7
Thomas Prescott <tcprescott@gmail.com>
2020-10-16 23:11:57 -0500

    more sgl rollers

---
+++ alttprbot/alttprgen/randomizer/ootr.py
@@ -0,0 +1,22 @@
+import aiohttp
+import os
+
+OOTR_BASE_URL = os.environ.get(
+    'RACETIME_BASE_URL', 'https://ootrandomizer.com')
+OOTR_API_KEY = os.environ.get('OOTR_API_KEY')
+
+
+async def roll_ootr(settings, encrypt=True):
+    async with aiohttp.request(
+        method='post',
+        url=f"{OOTR_BASE_URL}/api/sglive/seed/create",
+        raise_for_status=True,
+        json=settings,
+        params={
+            "key": OOTR_API_KEY,
+            "encrypt": str(encrypt).lower()
+        }
+    ) as resp:
+        result = await resp.json()
+
+    return f"https://ootrandomizer.com/seed/get?id={result['id']}"
---
+++ alttprbot/alttprgen/randomizer/smdash.py
@@ -0,0 +1,5 @@
+import random
+
+
+def roll_smdash():
+    return random.randrange(1000000, 9999999)
