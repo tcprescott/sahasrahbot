a3041234b8d157fd2b22a19bc098d02b8a08cd9d
Thomas Prescott <tcprescott@gmail.com>
2020-10-20 17:57:37 -0500

    use tenacity to retry in case of some sort of exception

--- alttprbot_racetime/bot.py
+++ alttprbot_racetime/bot.py
@@ -7,7 +7,8 @@ import sys
 from functools import partial
 
 import aiohttp
-from alttprbot.exceptions import SahasrahBotException
+from aiohttp.client_exceptions import ClientResponseError
+from tenacity import RetryError, AsyncRetrying, stop_after_attempt, retry_if_exception_type
 from bs4 import BeautifulSoup
 from config import Config as c
 from racetime_bot import Bot
@@ -64,7 +65,8 @@ class SahasrahBotRaceTimeBot(Bot):
             await asyncio.sleep(1)
             count += 1
             if count > 30:
-                raise Exception("Bot took too long to establish connection with room websocket.")
+                raise Exception(
+                    "Bot took too long to establish connection with room websocket.")
 
         return handler
 
@@ -73,49 +75,54 @@ class SahasrahBotRaceTimeBot(Bot):
 
     async def create_race(self, config):
         try:
-            async with aiohttp.request(
-                method='get',
-                url=f'{RTGG_BASE_URL}/{self.category_slug}/startrace',
-                cookies={'sessionid': RTGG_SESSION_TOKEN},
-                raise_for_status=True
-            ) as resp:
-                soup = BeautifulSoup(await resp.text(), features="html5lib")
-                cookies = resp.cookies
-        except:
-            raise SahasrahBotException(
-                "Unable to acquire CSRF token.  Please contact Synack for help.")
+            async for attempt in AsyncRetrying(stop=stop_after_attempt(5), retry=retry_if_exception_type(ClientResponseError)):
+                with attempt:
+                    async with aiohttp.request(
+                        method='get',
+                        url=f'{RTGG_BASE_URL}/{self.category_slug}/startrace',
+                        cookies={'sessionid': RTGG_SESSION_TOKEN},
+                        raise_for_status=True
+                    ) as resp:
+                        soup = BeautifulSoup(await resp.text(), features="html5lib")
+                        cookies = resp.cookies
+        except RetryError as e:
+            raise e.last_attempt._exception from e
 
-        csrftoken = soup.find('input', {'name': 'csrfmiddlewaretoken'})['value']
+        csrftoken = soup.find(
+            'input', {'name': 'csrfmiddlewaretoken'})['value']
 
         config['csrfmiddlewaretoken'] = csrftoken
         config['recordable'] = True
 
+        # if we're developing locally, always make the race unlisted to keep things tidy to users
+        if c.DEBUG:
+            config['unlisted'] = 'on'
+
         cookies['sessionid'] = RTGG_SESSION_TOKEN
 
         try:
-            async with aiohttp.request(
-                method='post',
-                url=f'{RTGG_BASE_URL}/{self.category_slug}/startrace',
-                cookies=cookies,
-                headers={'Origin': RTGG_BASE_URL,
-                        'Referer': f'{RTGG_BASE_URL}/{self.category_slug}/startrace'},
-                allow_redirects=False,
-                raise_for_status=True,
-                data=config
-            ) as resp:
-                print(await resp.text())
-                if resp.status == 302:
-                    room_path = resp.headers['Location']
-        except:
-            # raise SahasrahBotException(
-            #     "Unable to create the race room.  Please contact Synack for help.")
-            print(await resp.text())
-            raise
+            async for attempt in AsyncRetrying(
+                    stop=stop_after_attempt(5),
+                    retry=retry_if_exception_type(ClientResponseError)):
+                with attempt:
+                    async with aiohttp.request(
+                        method='post',
+                        url=f'{RTGG_BASE_URL}/{self.category_slug}/startrace',
+                        cookies=cookies,
+                        headers={'Origin': RTGG_BASE_URL,
+                                 'Referer': f'{RTGG_BASE_URL}/{self.category_slug}/startrace'},
+                        allow_redirects=False,
+                        raise_for_status=True,
+                        data=config
+                    ) as resp:
+                        if resp.status == 302:
+                            room_path = resp.headers['Location']
+        except RetryError as e:
+            raise e.last_attempt._exception from e
 
         race_handler = await self.create_handler_by_room_name(room_path)
 
         return race_handler
-
 
     async def start(self):
         self.loop.create_task(self.reauthorize())
