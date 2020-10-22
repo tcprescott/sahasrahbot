4152725f3e820d2b60d755d2a0938ba3a34b22e6
Thomas Prescott <tcprescott@gmail.com>
2020-10-22 01:11:18 -0500

    add league roles command

--- alttprbot_discord/cogs/league.py
+++ alttprbot_discord/cogs/league.py
@@ -133,7 +133,6 @@ class League(commands.Cog):
     @commands.check_any(commands.has_permissions(manage_roles=True), commands.is_owner())
     @restrict_league_server()
     async def importleagueroles(self, ctx):
-
         async with aiohttp.request(
             method='get',
             url='https://alttprleague.com/json_ep/roster/'
@@ -148,6 +147,51 @@ class League(commands.Cog):
             await update_division(ctx, division=division, pendant_roles=pendant_roles)
 
 
+    @commands.command()
+    @commands.check_any(commands.has_permissions(manage_roles=True), commands.is_owner())
+    @restrict_league_server()
+    async def leagueroles(self, ctx, member_tag):
+        async with aiohttp.request(
+            method='get',
+            url='https://alttprleague.com/json_ep/player/',
+            params={
+                'discord': member_tag,
+            },
+            raise_for_status=True
+        ) as resp:
+            r = await resp.json()
+            player = r['results'][0]
+
+        async with aiohttp.request(
+            method='get',
+            url='https://alttprleague.com/json_ep/team/',
+            params={
+                'name': player['team_name'],
+            },
+            raise_for_status=True
+        ) as resp:
+            r = await resp.json()
+            team = r['results'][0]
+
+        pendant = player['position'].lower()
+        division_role = await find_or_create_role(ctx, f"Division - {player['division_name']}")
+        player_role = await find_or_create_role(
+            ctx,
+            "Racer" if team['invitational'] else "Open Racer"
+        )
+        team_role = await find_or_create_role(ctx, player['team_name'])
+        pendant_role = await find_or_create_role(ctx, pendant)
+
+        await update_player(
+            ctx,
+            team=team,
+            division_role=division_role,
+            player_role=player_role,
+            team_role=team_role,
+            pendant=pendant,
+            pendant_role=pendant_role
+        )
+
 async def update_division(ctx, division, pendant_roles):
     division_role = await find_or_create_role(ctx, f"Division - {division['name']}")
     player_role = await find_or_create_role(
@@ -155,45 +199,51 @@ async def update_division(ctx, division, pendant_roles):
         "Racer" if division['invitational'] else "Open Racer"
     )
     for team in division['teams']:
-        team_role = await find_or_create_role(ctx, team['name'])
+        await update_team(ctx, team, division_role=division_role, player_role=player_role, pendant_roles=pendant_roles)
 
-        for pendant in ['courage', 'wisdom', 'power']:
-            if c.DEBUG:
-                await asyncio.sleep(random.uniform(0, 3))
-                print(
-                    f"would have added \"{team[pendant]['discord']}\" to role \"{division_role.name}\"")
-                print(
-                    f"would have added \"{team[pendant]['discord']}\" to role \"{player_role.name}\"")
-                print(
-                    f"would have added \"{team[pendant]['discord']}\" to role \"{team_role.name}\"")
-                print(
-                    f"would have added \"{team[pendant]['discord']}\" to role \"{pendant_roles[pendant].name}\"")
-            else:
-                team_member = ctx.guild.get_member_named(
-                    team[pendant]['discord'])
-                if team_member is None:
-                    await ctx.send(f"Could not resolve user {team[pendant]['discord']}, skipping...")
-                    continue
-                await team_member.add_roles(division_role, player_role, team_role, pendant_roles[pendant])
-                await srlnick.insert_twitch_name(team_member.id, team[pendant]['twitch_name'])
-                await srlnick.insert_rtgg_id(team_member.id, team[pendant]['rtgg_id'])
 
-                if os.environ.get("LEAGUE_SUBMIT_GAME_SECRET"):
-                    if not team[pendant].get('discord_id', None) == team_member.id:
-                        async with aiohttp.request(
-                            method='post',
-                            url='https://alttprleague.com/json_ep/player/',
-                            data={
-                                'id': team[pendant]['id'],
-                                'discord_id': team_member.id,
-                                'secret': os.environ.get("LEAGUE_SUBMIT_GAME_SECRET")
-                            }
-                        ) as _:
-                            pass
-                else:
-                    print(
-                        f"Would have updated \"{team[pendant]['discord']}\" ({team[pendant]['id']}) to discord id {team_member.id}")
+async def update_team(ctx, team, division_role, player_role, pendant_roles):
+    team_role = await find_or_create_role(ctx, team['name'])
 
+    for pendant in ['courage', 'wisdom', 'power']:
+        await update_player(ctx, team, division_role, player_role, team_role, pendant, pendant_roles[pendant])
+
+async def update_player(ctx, team, division_role, player_role, team_role, pendant, pendant_role):
+    if c.DEBUG:
+        await asyncio.sleep(random.uniform(0, 3))
+        print(
+            f"would have added \"{team[pendant]['discord']}\" to role \"{division_role.name}\"")
+        print(
+            f"would have added \"{team[pendant]['discord']}\" to role \"{player_role.name}\"")
+        print(
+            f"would have added \"{team[pendant]['discord']}\" to role \"{team_role.name}\"")
+        print(
+            f"would have added \"{team[pendant]['discord']}\" to role \"{pendant_role.name}\"")
+    else:
+        team_member = ctx.guild.get_member_named(
+            team[pendant]['discord'])
+        if team_member is None:
+            await ctx.send(f"Could not resolve user {team[pendant]['discord']}, skipping...")
+            return
+        await team_member.add_roles(division_role, player_role, team_role, pendant_role)
+        await srlnick.insert_twitch_name(team_member.id, team[pendant]['twitch_name'])
+        await srlnick.insert_rtgg_id(team_member.id, team[pendant]['rtgg_id'])
+
+        if os.environ.get("LEAGUE_SUBMIT_GAME_SECRET"):
+            if not team[pendant].get('discord_id', None) == team_member.id:
+                async with aiohttp.request(
+                    method='post',
+                    url='https://alttprleague.com/json_ep/player/',
+                    data={
+                        'id': team[pendant]['id'],
+                        'discord_id': team_member.id,
+                        'secret': os.environ.get("LEAGUE_SUBMIT_GAME_SECRET")
+                    }
+                ) as _:
+                    pass
+        else:
+            print(
+                f"Would have updated \"{team[pendant]['discord']}\" ({team[pendant]['id']}) to discord id {team_member.id}")
 
 async def find_or_create_role(ctx, role_name):
     try:
