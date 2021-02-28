772b57407fafa8e648457c042ca51f178c41b09d
Thomas Prescott <tcprescott@gmail.com>
2021-02-28 13:12:24 -0600

    fixes

--- alttprbot/util/speedgaming.py
+++ alttprbot/util/speedgaming.py
@@ -25,7 +25,7 @@ async def get_upcoming_episodes_by_event(event, hours_past=4, hours_future=4):
             test_schedule.append(episode)
         return test_schedule
 
-    now = datetime.utc_now()
+    now = datetime.now(tz=pytz.timezone('US/Eastern'))
     sched_from = now - timedelta(hours=hours_past)
     sched_to = now + timedelta(hours=hours_future)
     params = {
