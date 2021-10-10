8d9e71e13993a6cc564bc6377e83d8976276f058
Thomas Prescott <tcprescott@gmail.com>
2021-10-10 12:11:23 -0500

    add bad id reporting

--- alttprbot_discord/cogs/tournament.py
+++ alttprbot_discord/cogs/tournament.py
@@ -4,6 +4,7 @@ import logging
 import discord
 from discord.ext import commands, tasks
 
+from alttprbot.tournament.core import TournamentConfig
 from alttprbot import tournaments
 from alttprbot.util import speedgaming
 from config import Config as c
@@ -19,6 +20,7 @@ class Tournament(commands.Cog):
         self.record_races.start()
         self.scheduling_needs.start()
         self.find_unsubmitted_races.start()
+        self.find_races_with_bad_discord.start()
 
     @tasks.loop(minutes=0.25 if c.DEBUG else 5, reconnect=True)
     async def create_races(self):
@@ -71,6 +73,16 @@ class Tournament(commands.Cog):
                             f"There was an error while sending a submission reminder for episode `{episode['id']}`.\n\n{str(e)}",
                             allowed_mentions=discord.AllowedMentions(everyone=True)
                         )
+
+    @tasks.loop(minutes=0.25 if c.DEBUG else 240, reconnect=True)
+    async def find_races_with_bad_discord(self):
+        logging.info('scanning for races with bad discord info')
+        for event_slug, tournament_class in tournaments.TOURNAMENT_DATA.items():
+            messages = await self.report_bad_player_discord(event_slug=event_slug)
+            event_data: TournamentConfig = await tournament_class.get_config()
+
+            if messages and event_data.audit_channel:
+                await event_data.audit_channel.send("\n".join(messages))
 
     @tasks.loop(minutes=0.25 if c.DEBUG else 15, reconnect=True)
     async def scheduling_needs(self):
@@ -183,6 +195,11 @@ class Tournament(commands.Cog):
         logging.info('tournament find_unsubmitted_races loop waiting...')
         await self.bot.wait_until_ready()
 
+    @find_races_with_bad_discord.before_loop
+    async def before_find_races_with_bad_discord(self):
+        logging.info('tournament find_races_with_bad_discord loop waiting...')
+        await self.bot.wait_until_ready()
+
     async def cog_check(self, ctx):  # pylint: disable=invalid-overridden-method
         if ctx.guild is None:
             return False
@@ -198,6 +215,31 @@ class Tournament(commands.Cog):
     @commands.is_owner()
     async def tourneyrace(self, ctx, event_slug, episode_number: int):
         await tournaments.create_tournament_race_room(event_slug, episode_number)
+
+    async def report_bad_player_discord(self, event_slug):
+        tournament_class = tournaments.TOURNAMENT_DATA[event_slug]
+
+        event_data: TournamentConfig = await tournament_class.get_config()
+        episodes = await speedgaming.get_upcoming_episodes_by_event(event_slug, hours_past=0, hours_future=48)
+
+        messages = []
+
+        for episode in episodes:
+            for match in ['match1', 'match2']:
+                if episode[match]:
+                    for player in episode[match]['players']:
+                        try:
+                            member = event_data.guild.get_member(int(player.get('discordId', '')))
+                        except ValueError:
+                            member = None
+
+                        if member is None:
+                            member =  event_data.guild.get_member_named(player.get('discordTag', ''))
+
+                        if member is None:
+                            messages.append(f"Episode {episode['id']} - {event_slug} - {player['displayName']} could not be found")
+
+        return messages
 
 def setup(bot):
     bot.add_cog(Tournament(bot))
