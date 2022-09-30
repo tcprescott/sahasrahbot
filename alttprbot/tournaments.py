113bea82fc15bc93cb1e7e6a3a88c56749ca3293
tsigma6 <tfitch89@gmail.com>
2022-09-30 11:48:28 -0400

    Updates for League Season 5 (and beyond).

--- alttprbot/tournaments.py
+++ alttprbot/tournaments.py
@@ -48,8 +48,8 @@ else:
         'smz3': dailies.SMZ3DailyRace,
 
         # ALTTPR League
-        # 'invleague': alttprleague.ALTTPRLeague,
-        # 'alttprleague': alttprleague.ALTTPROpenLeague,
+        'invleague': alttprleague.ALTTPRLeague,
+        'alttprleague': alttprleague.ALTTPROpenLeague,
 
         # SGL
         # 'sgl21alttpr': sgl21.ALTTPRBrackets,
