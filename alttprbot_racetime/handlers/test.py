29f74a0c06b76c89c64ff9cd340337a5585e2d98
Thomas Prescott <tcprescott@gmail.com>
2023-01-06 23:24:23 -0600

    fix for long running commands

--- alttprbot_racetime/handlers/test.py
+++ alttprbot_racetime/handlers/test.py
@@ -1,5 +1,16 @@
+import asyncio
 from .alttpr import GameHandler as TestGameHandler
 
 
 class GameHandler(TestGameHandler):
-    pass
+    async def ex_test(self, args, message):
+        try:
+            seconds = int(args[0])
+        except IndexError:
+            seconds = 30
+        except ValueError:
+            await self.send_message("Invalid number of seconds")
+            return
+        await self.send_message(f"initated test command, waiting {seconds} seconds")
+        await asyncio.sleep(seconds)
+        await self.send_message("test command complete")
