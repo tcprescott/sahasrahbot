855f649339abdcbe4244efe198b45e6bea3d9dcd
Thomas Prescott <tcprescott@gmail.com>
2023-03-08 23:11:34 -0600

    migration sql

--- helpers/migrate_srlnick.sql
+++ helpers/migrate_srlnick.sql
@@ -1,12 +1,12 @@
-INSERT into users (discord_user_id, twitch_name, rtgg_id, display_name) SELECT discord_user_id, twitch_name, rtgg_id, display_name FROM srlnick;
+INSERT IGNORE into users (discord_user_id, twitch_name, rtgg_id) SELECT discord_user_id, twitch_name, rtgg_id FROM srlnick;
 
 UPDATE asynctournamentrace
 INNER JOIN users ON users.discord_user_id = asynctournamentrace.discord_user_id
 SET asynctournamentrace.user_id = users.id;
 
 UPDATE asynctournamentauditlog
-INNER JOIN users ON users.discord_user_id = asynctournamentrace.discord_user_id
-SET asynctournamentrace.user_id = users.id;
+INNER JOIN users ON users.discord_user_id = asynctournamentauditlog.discord_user_id
+SET asynctournamentauditlog.user_id = users.id;
 
 UPDATE asynctournamentwhitelist
 INNER JOIN users ON users.discord_user_id = asynctournamentwhitelist.discord_user_id
