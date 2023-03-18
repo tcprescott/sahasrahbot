65ad4a89ce89eb0f1a5febde02da98c64ecef68c
Thomas Prescott <tcprescott@gmail.com>
2023-03-18 12:24:18 -0500

    better schedule log event

--- alttprbot/speedgaming.py
+++ alttprbot/speedgaming.py
@@ -134,8 +134,9 @@ async def get_upcoming_episodes_by_event(event, hours_past=4, hours_future=4):
         url=f'{c.SgApiEndpoint}/schedule',
         params=params,
     ) as resp:
-        logging.info(resp.url)
         schedule = await resp.json(content_type='text/html')
+        episode_ids = [episode['id'] for episode in schedule]
+        logging.info(f'Retrieved schedule for {event} ({resp.status} {resp.reason}).  Received {len(schedule)} matches.  From: {sched_from} To: {sched_to}.  Match IDs: {", ".join(episode_ids)}')
 
     if 'error' in schedule:
         raise SGEventNotFoundException(f"Unable to retrieve schedule for {event}. {schedule.get('error')}")
