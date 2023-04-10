d45a7a17a634b6394ca9f724e1344b49de87381e
Thomas Prescott <tcprescott@gmail.com>
2023-04-10 07:40:52 -0500

    set league to week 6

--- alttprbot/tournament/smrl.py
+++ alttprbot/tournament/smrl.py
@@ -18,7 +18,7 @@ from alttprbot_discord.util.smvaria_discord import SuperMetroidVariaDiscord
 # 6. DASH recall https: // dashrando.net/
 # 7. Coop: Full Item Shuffle, Full Area Shuffle, Boss Shuffle
 
-WEEK = 5
+WEEK = 6
 
 WEEKS = {
     1: {
