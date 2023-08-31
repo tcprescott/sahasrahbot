64a125d948873d0faa5ea3f2d306075ad9e013be
Thomas Prescott <tcprescott@gmail.com>
2023-08-30 20:58:48 -0500

    use tournament branch

--- alttprbot/tournament/boots.py
+++ alttprbot/tournament/boots.py
@@ -1,12 +1,12 @@
 from alttprbot.tournament.alttpr import ALTTPRTournamentRace
 from alttprbot.tournament.core import TournamentConfig
-from alttprbot.alttprgen import preset
+from alttprbot.alttprgen import generator
 from alttprbot_discord.bot import discordbot
 
 
 class ALTTPRCASBootsTournamentRace(ALTTPRTournamentRace):
     async def roll(self):
-        self.seed, self.preset_dict = await preset.get_preset('casualboots', allow_quickswap=True)
+        self.seed = await generator.ALTTPRPreset('casualboots').generate(allow_quickswap=True, tournament=True, hints=False, spoilers="off", branch='tournament')
 
     async def configuration(self):
         guild = discordbot.get_guild(973765801528139837)
