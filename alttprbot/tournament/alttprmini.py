556a2d32d2d9f2c4863ccd9d4d267a6f6de5e408
Thomas Prescott <tcprescott@gmail.com>
2023-09-01 19:11:46 -0500

    setup alttprmini

--- alttprbot/tournament/alttprmini.py
+++ alttprbot/tournament/alttprmini.py
@@ -1,32 +1,11 @@
-from alttprbot.alttprgen.generator import ALTTPRMystery
+from alttprbot.alttprgen.generator import ALTTPRPreset
 from alttprbot.tournament.alttpr import ALTTPRTournamentRace
 from alttprbot.tournament.core import TournamentConfig
 from alttprbot_discord.bot import discordbot
 
-# ALTTPRMINI_TITLE_MAP = {
-#     'Open Deutscher Hard Pool': 'derduden2/german_hard',
-#     'Open 6/6 Fast Ganon': 'open_fast_66',
-#     'Open All Dungeons': 'dungeons',
-#     'Standard': 'standard',
-#     'Casual Boots': 'casualboots',
-#     'Boss Shuffle': 'enemizer_bossesonly',
-#     'Enemy Shuffle Assured': 'teto/enemizer_assured',
-#     'Big Key Shuffle': 'catobat/bkshuffle',
-#     'Keysanity': 'keysanity',
-# }
-
-
 class ALTTPRMiniTournament(ALTTPRTournamentRace):
     async def roll(self):
-        # try:
-        #     preset = ALTTPRMINI_TITLE_MAP[self.episode['match1']['title']]
-        # except KeyError:
-        #     await self.rtgg_handler.send_message("Invalid mode chosen, please contact a tournament admin for assistance.")
-        #     raise
-        # self.seed = await ALTTPRPreset(preset).generate(hints=False, spoilers="off", allow_quickswap=True)
-
-        mystery = await ALTTPRMystery("tournament_de").generate(tournament=True, spoilers="mystery")
-        self.seed = mystery.seed
+        self.seed = await ALTTPRPreset("nightcl4w/duality").generate(tournament=True, spoilers="off", branch='tournament')
 
 
     async def configuration(self):
@@ -34,7 +13,7 @@ class ALTTPRMiniTournament(ALTTPRTournamentRace):
         return TournamentConfig(
             guild=guild,
             racetime_category='alttpr',
-            racetime_goal='Beat the game',
+            racetime_goal='Beat the game (assisted)',
             event_slug="alttprmini",
             audit_channel=discordbot.get_channel(473668481011679234),
             commentary_channel=discordbot.get_channel(469317757331308555),
@@ -44,6 +23,7 @@ class ALTTPRMiniTournament(ALTTPRTournamentRace):
                 guild.get_role(623071415129866240),
             ],
             lang='de',
-            stream_delay=10,
+            # stream_delay=10,
             gsheet_id='1dWzbwxoErGQyO4K1tZ-EexX1bdnTGuxQhLJDnmfcaR4',
+            coop=True,
         )
