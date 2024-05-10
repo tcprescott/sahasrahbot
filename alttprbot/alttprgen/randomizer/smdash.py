2fc1a53462812353ed17244032dc168eb0d84214
MassHesteria <brad.hester@gmail.com>
2024-05-10 16:19:46 -0500

    prep for dash spoilers

--- alttprbot/alttprgen/randomizer/smdash.py
+++ alttprbot/alttprgen/randomizer/smdash.py
@@ -1,9 +1,13 @@
 import aiohttp
+import ssl
 
 
-async def create_smdash(mode="classic_mm"):
+async def create_smdash(mode="classic_mm", spoiler=False):
     async with aiohttp.ClientSession() as session:
-        async with session.get(f'https://www.dashrando.net/generate/{mode}', allow_redirects=False) as resp:
+        route = f'https://www.dashrando.net/generate/{mode}?race=1'
+        if spoiler:
+            route += '&spoiler=1'
+        async with session.get(route, ssl=ssl.SSLContext(protocol=ssl.PROTOCOL_TLSv1_2), allow_redirects=False) as resp:
             msg = await resp.text()
             if resp.status == 307:
                 return msg
